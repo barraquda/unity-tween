1d634abc034bd48fb47f51afeffc35ed128a65e7
Shohei Miyashita <shohei.miyashita@kamasu.jp>
2016-05-01 00:08:56 +0900

    init

---
+++ UguiExtensions.cs
@@ -0,0 +1,403 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Barracuda;
+using UnityEngine.UI;
+
+namespace Barracuda.UISystem
+{
+
+    public enum PropKey
+    {
+        Left, Bottom, Scale, ScaleX, ScaleY, RotationX, RotationY, RotationZ, Opacity, OpacityChildren, X, Y, GlobalX, GlobalY, Width, Height, Size, Brightness, Alpha, CanvasRendererAlpha
+    }
+
+    public abstract class UGUIProp
+    {
+        public PropKey Key { get; private set; }
+        public float Value { get; private set; }
+
+        protected UGUIProp (PropKey key, float val)
+        {
+            this.Key = key;
+            this.Value = val;
+        }
+    }
+
+    public class Relative : UGUIProp
+    {
+        public Relative (PropKey key, float val) : base(key, val) {}
+    }
+
+    public class Absolute : UGUIProp
+    {
+        public Absolute (PropKey key, float val) : base(key, val) {}
+    }
+
+    public class Multiple : UGUIProp
+    {
+        public Multiple (PropKey key, float val) : base(key, val) {}
+    }
+
+    public static class UguiExtensions
+    {
+        //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ Ugui Extension  /_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_//
+
+        public static void Fix(this RectTransform ui, params UGUIProp[] parameters)
+        {
+            foreach (UGUIProp prop in parameters)
+            {
+                MatchProp(ui, prop)(1.0f);
+            }
+        }
+
+        public static TweenSet Animate(this RectTransform ui, UGUIProp[] parameters , float duration, EasingMode easingMode = null, Action onFinish = null, float offset = 0.0f)
+        {
+            var actions = new Action<float>[parameters.Length];
+            for (int i=0; i<parameters.Length; i++)
+            {
+                actions[i] = MatchProp(ui, parameters[i]);
+            }
+            var animationSet = new TweenSet(ui, duration, actions, easingMode, onFinish, offset);
+            UguiTweenManager.Add(animationSet);
+            return animationSet;
+        }
+
+        public static TweenSet Animate(this RectTransform ui, UGUIProp parameter , float duration, EasingMode easingMode = null, Action onFinish = null, float offset = 0.0f)
+        {
+            var action = new Action<float>[]{ MatchProp(ui, parameter) };
+            var animationSet = new TweenSet(ui, duration, action, easingMode, onFinish, offset);
+            UguiTweenManager.Add(animationSet);
+            return animationSet;
+        }
+
+        //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ Ugui Optional Extension /_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_//
+
+        public static TweenSet Alpha(this RectTransform ui, float alpha, float duration, Action onFinish = null, float offset = 0.0f)
+        {
+            return ui.Animate(new Absolute(PropKey.Alpha, alpha), duration, Easing.EaseOut, onFinish, offset);
+        }
+
+        public static TweenSet Opacity(this RectTransform ui, float opacity, float duration, Action onFinish = null, float offset = 0.0f)
+        {
+            return ui.Animate(new Absolute(PropKey.Opacity, opacity), duration, Easing.EaseOut, onFinish, offset);
+        }
+
+
+        //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/ Ugui Extension Utility /_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_//
+
+        private static Graphic GetGraphicFromGameObject(GameObject target)
+        {
+            var image = target.GetComponent<Image>();
+            if (image != null)
+            {
+                return image;
+            }
+
+            var text = target.GetComponent<Text>();
+            return text;
+        }
+
+        private static void SetOpacity(Graphic g, float opacity)
+        {
+            if (g != null)
+            {
+                g.color = new Color(g.color.r, g.color.g, g.color.b, opacity);
+            }
+        }
+
+        private static void CollectGraphics(List<Graphic> list, GameObject target)
+        {
+            var g = GetGraphicFromGameObject(target);
+            if (g != null)
+            {
+                list.Add(g);
+            }
+            for(var i = 0; i < target.transform.childCount; i++)
+            {
+                var element = target.transform.GetChild(i);
+                CollectGraphics(list, element.gameObject);
+            }
+        }
+
+
+        internal static Action<float> MatchProp(RectTransform ui, UGUIProp prop)
+        {
+            if (prop is Absolute)
+            {
+                switch (prop.Key)
+                {
+                    case PropKey.Width:
+                    {
+                        var prevWidth = ui.sizeDelta.x;
+                        var diff = prop.Value - prevWidth;
+                        return degree => {
+                            ui.sizeDelta = new Vector2(prevWidth + diff * degree, ui.sizeDelta.y);
+                        };
+                    }
+                    case PropKey.X:
+                    {
+                        var prevX = ui.anchoredPosition.x;
+                        var diffX = prop.Value - prevX;
+                        return degree => {
+                            ui.anchoredPosition = new Vector2(prevX + diffX * degree, ui.anchoredPosition.y);
+                        };
+                    }
+                    case PropKey.Y:
+                    {
+                        var prevY = ui.anchoredPosition.y;
+                        var diffY = prop.Value - prevY;
+                        return degree => {
+                            ui.anchoredPosition = new Vector2(ui.anchoredPosition.x, prevY + diffY * degree);
+                        };
+                    }
+                    case PropKey.GlobalX:
+                    {
+                        var prevX = ui.position.x;
+                        var diffX = prop.Value - prevX;
+                        return degree => {
+                            ui.position = new Vector2(prevX + diffX * degree, ui.position.y);
+                        };
+                    }
+                    case PropKey.GlobalY:
+                    {
+                        var prevY = ui.position.y;
+                        var diffY = prop.Value - prevY;
+                        return degree => {
+                            ui.position = new Vector2(ui.position.x, prevY + diffY * degree);
+                        };
+                    }
+                    case PropKey.Left:
+                    {
+                        var targetLeft = prop.Value;
+                        var prevMinX = ui.anchorMin.x;
+                        var prevMaxX = ui.anchorMax.x;
+                        var diffMinX = targetLeft - prevMinX;
+                        var diffMaxX = targetLeft - prevMaxX + (ui.anchorMax.x - ui.anchorMin.x);
+                        return degree => {
+                            var nowMax = ui.anchorMax;
+                            var nowMin = ui.anchorMin;
+                            ui.anchorMax = new Vector2(prevMaxX + (diffMaxX * degree), nowMax.y);
+                            ui.anchorMin = new Vector2(prevMinX + (diffMinX * degree), nowMin.y);
+                        };
+                    }
+                    case PropKey.Bottom:
+                    {
+                        var targetLeft = prop.Value;
+                        var prevMinY = ui.anchorMin.y;
+                        var prevMaxY = ui.anchorMax.y;
+                        var diffMinY = targetLeft - prevMinY;
+                        var diffMaxY = targetLeft - prevMaxY + (ui.anchorMax.y - ui.anchorMin.y);
+                        return degree => {
+                            var nowMax = ui.anchorMax;
+                            var nowMin = ui.anchorMin;
+                            ui.anchorMax = new Vector2(nowMax.x, prevMaxY + (diffMaxY * degree));
+                            ui.anchorMin = new Vector2(nowMin.x, prevMinY + (diffMinY * degree));
+                        };
+                    }
+                    case PropKey.RotationZ:
+                    {
+                        var rotationZ = prop.Value;
+                        var prevRotation = ui.localRotation.eulerAngles;
+                        var diff = (rotationZ - prevRotation.z) % 360;
+                        return degree => {
+                            var v = new Vector3(prevRotation.x, prevRotation.y, prevRotation.z + diff * degree);
+                            ui.localRotation = Quaternion.Euler(v);
+                        };
+                    }
+                    case PropKey.RotationY:
+                    {
+                        var rotationY = prop.Value;
+                        var prevRotation = ui.localRotation.eulerAngles;
+                        var diff = (rotationY - prevRotation.y) % 360;
+                        return degree => {
+                            var v = new Vector3(prevRotation.x, prevRotation.y + diff * degree, prevRotation.y);
+                            ui.localRotation = Quaternion.Euler(v);
+                        };
+                    }
+                    case PropKey.RotationX:
+                    {
+                        var rotationX = prop.Value;
+                        var prevRotation = ui.localRotation.eulerAngles;
+                        var diff = (rotationX - prevRotation.x) % 360;
+                        return degree => {
+                            var v = new Vector3(prevRotation.x + diff * degree, prevRotation.y, prevRotation.z);
+                            ui.localRotation = Quaternion.Euler(v);
+                        };
+                    }
+                    case PropKey.Scale:
+                    {
+                        var prevScale = ui.localScale;
+                        var diffScale = new Vector3(1, 1, 1) * prop.Value - prevScale;
+                        return degree => {
+                            ui.localScale = prevScale + diffScale * degree;
+                        };
+                    }
+                    case PropKey.ScaleX:
+                    {
+                        var prevScaleX = ui.localScale.x;
+                        var diffScaleX = prop.Value - prevScaleX;
+                        return degree => {
+                            ui.localScale = new Vector3(prevScaleX + diffScaleX * degree, ui.localScale.y, ui.localScale.z);
+                        };
+                    }
+                    case PropKey.ScaleY:
+                    {
+                        var prevScaleY = ui.localScale.y;
+                        var diffScaleY = prop.Value - prevScaleY;
+                        return degree => {
+                            ui.localScale = new Vector3(ui.localScale.x, prevScaleY + diffScaleY * degree, ui.localScale.z);
+                        };
+                    }
+                    case PropKey.Brightness:
+                    {
+                        var g = GetGraphicFromGameObject(ui.gameObject);
+                        var bright = g.color.grayscale;
+                        var diff = prop.Value - bright;
+                        return degree => {
+                            var d = bright + diff * degree;
+                            g.color = new Color(d, d, d, g.color.a);
+                        };
+                    }
+                    case PropKey.Opacity:
+                    {
+                        var g = GetGraphicFromGameObject(ui.gameObject);
+                        if (g == null)
+                        {
+                            return degree => {};
+                        }
+                        var prevOpacity = g.color.a;
+                        var diff = prop.Value - prevOpacity;
+                        return degree => SetOpacity(g, prevOpacity + diff * degree);
+                    }
+                    case PropKey.OpacityChildren:
+                    {
+                        var gs = new List<Graphic>();
+                        CollectGraphics(gs, ui.gameObject);
+                        var opacity = prop.Value;
+                        var prevOpacities = new float[gs.Count];
+                        var index = 0;
+                        gs.ForEach(element => {
+                            prevOpacities[index] = element.color.a;
+                            index++;
+                        });
+                        return degree => {
+                            index = 0;
+                            gs.ForEach(element => {
+                                SetOpacity(element, prevOpacities[index] + (opacity - prevOpacities[index]) * degree);
+                                index++;
+                            });
+                        };
+                    }
+                    case PropKey.Alpha:
+                    {
+                        var cg = ui.GetComponent<CanvasGroup>();
+                        var prev = cg.alpha;
+                        var diff = prop.Value - prev;
+                        return degree => { cg.alpha = prev + diff * degree; };
+                    }
+                    case PropKey.CanvasRendererAlpha:
+                    {
+                        var cr = ui.GetComponent<CanvasRenderer>();
+                        var prev = cr.GetAlpha();
+                        var diff = prop.Value - prev;
+                        return degree => cr.SetAlpha(prev + diff * degree);
+                    }
+                }
+            }
+            else if (prop is Relative)
+            {
+                switch (prop.Key)
+                {
+                    case PropKey.X:
+                    {
+                        var prev = ui.anchoredPosition.x;
+                        var diff = prop.Value;
+                        return degree => {
+                            ui.anchoredPosition = new Vector2(prev + diff * degree, ui.anchoredPosition.y);
+                        };
+                    }
+                    case PropKey.Y:
+                    {
+                        var prev = ui.anchoredPosition.y;
+                        var diff = prop.Value;
+                        return degree => {
+                            ui.anchoredPosition = new Vector2(ui.anchoredPosition.x, prev + diff * degree);
+                        };
+                    }
+                    case PropKey.Left:
+                    {
+                        var left = prop.Value;
+                        var prevMinX = ui.anchorMin.x;
+                        var prevMaxX = ui.anchorMax.x;
+                        return degree => {
+                            var nowMax = ui.anchorMax;
+                            var nowMin = ui.anchorMin;
+                            ui.anchorMax = new Vector2(prevMaxX + left * degree, nowMax.y);
+                            ui.anchorMin = new Vector2(prevMinX + left * degree, nowMin.y);
+                        };
+                    }
+                    case PropKey.Bottom:
+                    {
+                        var bottom = prop.Value;
+                        var prevMinY = ui.anchorMin.y;
+                        var prevMaxY = ui.anchorMax.y;
+                        return degree => {
+                            var nowMax = ui.anchorMax;
+                            var nowMin = ui.anchorMin;
+                            ui.anchorMax = new Vector2(nowMax.x, prevMaxY + bottom * degree);
+                            ui.anchorMin = new Vector2(nowMin.x, prevMinY + bottom * degree);
+                        };
+                    }
+                    case PropKey.RotationZ:
+                    {
+                        var r = prop.Value;
+                        var prevRotation = ui.localRotation.eulerAngles;
+                        return degree => {
+                            ui.localRotation = Quaternion.Euler(new Vector3(
+                                ui.localRotation.x,
+                                ui.localRotation.y,
+                                prevRotation.z + r * degree));
+                        };
+                    }
+                }
+            }
+            else if (prop is Multiple)
+            {
+                switch (prop.Key)
+                {
+                    case PropKey.Scale:
+                    {
+                        var prevScale = ui.localScale;
+                        var diffScale = prop.Value * ui.localScale - prevScale;
+                        return degree => {
+                            ui.localScale = prevScale + diffScale * degree;
+                        };
+                    }
+                    case PropKey.ScaleX:
+                    {
+                        var prevScaleX = ui.localScale.x;
+                        var diffScaleX = prop.Value * ui.localScale.x - prevScaleX;
+                        return degree => {
+                            ui.localScale = new Vector2(prevScaleX + diffScaleX * degree, ui.localScale.y);
+                        };
+                    }
+                    case PropKey.ScaleY:
+                    {
+                        var prevScaleY = ui.localScale.y;
+                        var diffScaleY = prop.Value * ui.localScale.y - prevScaleY;
+                        return degree => {
+                            ui.localScale = new Vector2(ui.localScale.x ,prevScaleY + diffScaleY * degree);
+                        };
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentException("[UguiExtensions] properties is illegal");
+            }
+
+            return null;
+        }
+    }
+}
---
+++ UguiTweenManager.cs
@@ -0,0 +1,223 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using Barracuda;
+
+namespace Barracuda.UISystem
+{
+    class TweenData
+    {
+        public float Offset { get; set; }
+
+        public float Duration { get; private set; }
+        public EasingMode EasingMode  { get; private set; }
+        public Action OnFinish  { get; private set; }
+
+        private readonly Action<float>[] animations;
+        private readonly Action<float> animation;
+
+        public TweenData(float duration, Action<float>[] animations, EasingMode easingMode, Action onFinish, float offset)
+        {
+            if (duration.CompareTo(0) != 1) {
+                throw new ArgumentException("[TweenData] duration must be over 0");
+            }
+
+            Duration = duration;
+            Offset = offset;
+
+            if (animations.Length == 1) {
+                animation = animations[0];
+            } else {
+                this.animations = animations;
+            }
+
+            EasingMode = easingMode ?? Easing.Linear;
+            OnFinish = onFinish;
+        }
+
+        public TweenData(float duration, Action<float> animation, EasingMode easingMode, Action onFinish, float offset)
+        {
+            Duration = duration;
+            this.animation = animation;
+            Offset = offset;
+
+            EasingMode = easingMode ?? Easing.Linear;
+            OnFinish = onFinish;
+        }
+
+        public void Execute(float v)
+        {
+            if (animation != null) {
+                animation(v);
+            } else {
+                foreach (Action<float> a in animations) {
+                    a(v);
+                }
+            }
+        }
+    }
+
+    public class TweenSet
+    {
+        private TweenData targetAnimation;
+        private readonly Queue<Action> onCompletes;
+
+        private RectTransform ui;
+        private bool isAnimation;
+        private float time;
+
+        public TweenSet(RectTransform ui, float duration, Action<float>[] animations, EasingMode easingMode, Action onFinish, float offset)
+        {
+            onCompletes = new Queue<Action>();
+            targetAnimation = new TweenData(duration, animations, easingMode, onFinish, offset);
+            this.ui = ui;
+        }
+
+        public TweenSet Animate(UGUIProp[] parameters, float duration, EasingMode easingMode = null, Action onFinish = null, float offset = 0.0f)
+        {
+            onCompletes.Enqueue(() =>
+            {
+                var actions = new Action<float>[parameters.Length];
+                for (int i = 0; i < parameters.Length; i++) {
+                    actions[i] = UguiExtensions.MatchProp(ui, parameters[i]);
+                }
+                targetAnimation = new TweenData(duration, actions, easingMode, onFinish, offset);
+            });
+            return this;
+        }
+
+        public TweenSet Animate(UGUIProp parameter, float duration, EasingMode easingMode = null, Action onFinish = null, float offset = 0.0f)
+        {
+            onCompletes.Enqueue(() =>
+            {
+                var action = UguiExtensions.MatchProp(ui, parameter);
+                targetAnimation = new TweenData(duration, action, easingMode, onFinish, offset);
+            });
+            return this;
+        }
+
+        public void Execute()
+        {
+            isAnimation = true;
+            time = 0.0f;
+        }
+
+        public void Kill()
+        {
+            UguiTweenManager.Remove(this);
+        }
+
+        public void Finish()
+        {
+            time = targetAnimation.Duration;
+        }
+
+        public bool Invoke()
+        {
+            if (targetAnimation != null) {
+                if (isAnimation && targetAnimation.Offset > 0) {
+                    targetAnimation.Offset -= Time.deltaTime;
+                    return true;
+                }
+
+                if (isAnimation) {
+                    if (targetAnimation.Offset < time) {
+                        var now = targetAnimation.EasingMode(time, time, 0, 1, targetAnimation.Duration);
+                        targetAnimation.Execute(now);
+                    }
+                    time += Time.deltaTime;
+                }
+
+                if (time <= targetAnimation.Duration) {
+                    var now = targetAnimation.EasingMode(time, time, 0, 1, targetAnimation.Duration);
+                    targetAnimation.Execute(now);
+                } else if (time > targetAnimation.Duration) {
+                    targetAnimation.Execute(targetAnimation.EasingMode(targetAnimation.Duration, targetAnimation.Duration, 0, 1, targetAnimation.Duration));
+
+                    if (targetAnimation.OnFinish != null) {
+                        targetAnimation.OnFinish();
+                    }
+
+                    if (onCompletes == null) {
+                        return false;
+                    }
+
+                    if (onCompletes.Count > 0) {
+                        time = 0.0f;
+                        onCompletes.Dequeue()();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+    }
+
+    public class UguiTweenManager : MonoBehaviour
+    {
+        private static UguiTweenManager instance;
+        private LinkedList<TweenSet> animationSets;
+        private LinkedList<TweenSet> removeCandidates;
+
+        public static UguiTweenManager GetInstance()
+        {
+            return instance;
+        }
+
+        public bool IsAnimating {
+            get {
+                return animationSets.Count > 0;
+            }
+        }
+
+        public static void Init()
+        {
+            var obj = new GameObject();
+            obj.name = "UGUIAnimationManager";
+            instance = obj.AddComponent<UguiTweenManager>();
+            instance.animationSets = new LinkedList<TweenSet>();
+            instance.removeCandidates = new LinkedList<TweenSet>();
+        }
+
+        public static void Clear()
+        {
+            if (instance != null) {
+                instance.animationSets.Clear();
+            }
+        }
+
+        public static void Remove(TweenSet animationSet)
+        {
+            instance.animationSets.Remove(animationSet);
+        }
+
+        public static void Add(TweenSet animationSet)
+        {
+            if (instance == null) {
+                Init();
+            }
+            instance.animationSets.AddLast(animationSet);
+            animationSet.Execute();
+        }
+
+        void Update()
+        {
+            for (LinkedListNode<TweenSet> node = animationSets.First; node != null; node = node.Next) {
+                if (!node.Value.Invoke()) {
+                    removeCandidates.AddFirst(node.Value);
+                }
+            }
+
+            if (removeCandidates.Count > 0) {
+                for (LinkedListNode<TweenSet> node = removeCandidates.First; node != null; node = node.Next) {
+                    animationSets.Remove(node.Value);
+                }
+
+                removeCandidates.Clear();
+            }
+
+        }
+    }
+}
